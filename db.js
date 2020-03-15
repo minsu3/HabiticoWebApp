bff9175d722f424ba6e220c24afc049ea2a6850e
Min Su Kim <kim97minsu@gmail.com>
2020-03-14 18:38:26 -0700

    finished

---
+++ db.js
@@ -0,0 +1,34 @@
+const MongoClient = require("mongodb").MongoClient
+const ObjectID = require('mongodb').ObjectID
+const dbname = "crud_mongodb"
+const url = "mongodb://localhost:27017"
+const mongoOptions = {useNewUrlParser: true }
+
+const state = {
+  db : null // default - we dont have a datebase yet
+}
+
+const connect = (cb) => {
+  if(state.db) {
+    cb() // callback
+  } else {
+    MongoClient.connect(url, mongoOptions, (err,client) => {
+      if(err) {
+        cb(err)
+      } else {
+        state.db = client.db(dbname)
+        cb()
+      }
+    })
+  }
+}
+
+const getPrimaryKey = (_id) => {
+  return ObjectID(_id)
+}
+
+const getDB = () => {
+  return state.db
+}
+
+module.exports = {getDB, connect, getPrimaryKey}
