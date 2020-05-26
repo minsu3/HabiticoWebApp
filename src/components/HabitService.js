1575cbe1aa781ace2a7bf858d225d3a0cf5b5124
Min Su Kim <kim97minsu@gmail.com>
2020-05-25 19:25:24 -0700

    changed remote url

--- src/components/HabitService.js
+++ src/components/HabitService.js
@@ -20,6 +20,23 @@ const addHabit = (text) => {
     .catch((error) => console.log(error));
 };
 
+const updateHabit = (id, text, index) => {
+  const url = `https://vast-anchorage-73432.herokuapp.com/habits/${id}`;
+  const bodyObj = {
+    habit: text,
+  };
+  const param = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(bodyObj),
+  };
+  fetch(url, param)
+    .then((res) => res.json())
+    .catch((err) => console.log("Could not update habit \n", err));
+};
+
 
 
 export { addHabit }
