e211353203ee34c63418b8cdb1e0bc7866bd9860
Min Su Kim <kim97minsu@gmail.com>
2020-05-25 20:59:44 -0700

    Add exact path for list

--- src/config/routes.js
+++ src/config/routes.js
@@ -12,20 +12,23 @@ import Insert from '../components/Insert'
 export default (
   <div>
     <Switch>
-      <Route exact path='/' component={ Home } />
-      <Route path='/habitico/list' component={ List } />
+      <Route exact path="/habitico/list" component={List} />
       {/* <Route path='/list/:id' render={(props) => {
         return <HabitInfo id={props.match.params.id}/>
       }} /> */}
-      <Route exact path='/quit' component={ Quit } />
-      <Route path='/quit/:id' render={(props) => {
-        return <QuitInfo id={props.match.params.id}/>
-      }} />
-      <Route exact path='/insert' component={ Insert } />
-      <Route path='/insert/time' component={ Insert } />
-      <Route path='/insert/price' component={ Insert } />
-      <Route path='/about' component={ About } />
-      <Route path='/login' component={ Login } />
+      <Route exact path="/quit" component={Quit} />
+      <Route
+        path="/quit/:id"
+        render={(props) => {
+          return <QuitInfo id={props.match.params.id} />;
+        }}
+      />
+      <Route exact path="/insert" component={Insert} />
+      <Route path="/insert/time" component={Insert} />
+      <Route path="/insert/price" component={Insert} />
+      <Route path="/about" component={About} />
+      <Route path="/login" component={Login} />
+      <Route path="/" component={Home} />
     </Switch>
   </div>
-)
+);
