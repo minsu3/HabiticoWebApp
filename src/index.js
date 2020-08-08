3930efa2e14f9398b1456ac65ebbb757c1c2b710
Min Su Kim <kim97minsu@gmail.com>
2020-08-07 23:37:59 -0700

    fixed minor bugs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Main from './Main';
-// import List from "./List";
+import Main from './components/shared/Main.js';
 import { BrowserRouter } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as serviceWorker from './serviceWorker';
